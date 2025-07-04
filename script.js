3802c0d69c4327491c58f59429828ed2c81a2737
venug200717 <venugopalps2007@gmail.com>
2025-07-04 12:40:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function submit(){
+var fname=document.getElementById('fname')
+var lname=document.getElementById('lname')
+var Email=document.getElementById('Email')
+var Phonenumber=document.getElementById('Phonenumber')
+var Address=document.getElementById('Address')
+var fullname=fname.value+" "+lname.value
+console.log(fullname) 
+console.log(fname.value) 
+console.log(lname.value)        
+console.log(Email.value)
+console.log(Phonenumber.value)
+console.log(Address.value) 
+}
